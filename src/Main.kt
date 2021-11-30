b7d5ece91faec787954d402515ea639b81066c02
TunaPT <78658365+TunaPT@users.noreply.github.com>
2021-11-30 12:51:27 +0000

    Updated Functions & Others

--- src/Main.kt
+++ src/Main.kt
@@ -4,19 +4,44 @@ fun main() {
     var num = readLine()
     while (num != "0" && num != "1") {
         println("Invalid response.\n")
-        println(makeMenu())
         num = readLine()
     }
     if (num == "1") {
-        println("Enter player name?")
-        var name = readLine()
-        var lenght = name!!.length
-        while (lenght < 3) {
-            println("Invalid response.\n")
+
+        do {
             println("Enter player name?")
-            name = readLine()
-            lenght = name!!.length
-        }
+            val name = readLine()
+            if (!isNameValid(name, 3)) {
+                println("Invalid response.\n")
+            }
+        } while (!isNameValid(name, 3))
+
+        do {
+            println("Show legend (y/n)?")
+            val legend = readLine()!!
+            if (legend != "y" && legend != "n" && legend != "Y" && legend != "N") {
+                println("Invalid response.\n")
+            }
+        } while (legend != "y" && legend != "n" && legend != "Y" && legend != "N")
+
+        do {
+            println("How many lines?")
+            val line = readLine()!!
+            if (line != "1") {
+                println("Invalid response.\n")
+            }
+        } while (line != "1")
+
+        do {
+            println("How many columns?")
+            val column = readLine()!!
+            if (column < "3") {
+                println("Invalid response.\n")
+            }
+        } while (column < "3")
+
+        println("How many mines (press enter for default value)?")
+        val mines = readLine()?.toIntOrNull()?:3
     }
 }
 
@@ -24,10 +49,28 @@ fun makeMenu(): String = "1 - Start New Game\n0 - Exit Game"
 
 fun makeTerrain(numLines: Int, numColumns: Int, numMines: Int, showLegend: Boolean = true, withColor: Boolean = false): String = ""
 
-fun isNameValid(name: String?, minLength: Int): Boolean = false
+fun isNameValid(name: String?, minLength: Int): Boolean {
+    if (name != null) {
+        var position = 0
+        while (name[position] != ' ') {
+            position++
+        }
+        return name[2] != ' ' && " " in name && name[0].isUpperCase() && name[position+1].isUpperCase()
+    } else return false
+}
 
 fun calculateNumMinesForGameConfiguration(numLines: Int, numColumns: Int): Int? = 0
 
 fun createLegend(numColumns: Int): String = ""
 
 fun isValidGameMinesConfiguration(numLines: Int, numColumns: Int, numMines: Int): Boolean = false
+
+/*
+        if (name[2] != ' ' && " " in name && name[0].isUpperCase() && name[position+1].isUpperCase()) {
+            return true
+        } else {
+            return false
+        }
+
+        return name[2] != ' ' && " " in name && name[0].isUpperCase() && name[position+1].isUpperCase()
+ */
