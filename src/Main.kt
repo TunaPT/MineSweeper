0613c373f20972fa234f7c5561010d42418322f0
TunaPT <78658365+TunaPT@users.noreply.github.com>
2021-11-30 22:01:11 +0000

    7/14 - Update Functions

--- src/Main.kt
+++ src/Main.kt
@@ -2,6 +2,7 @@ fun main() {
     val invalid = "Invalid response.\n"
     var wantLegend = false
     var column = 0
+    var mines = 0
     do {
         println(makeMenu())
         val num = readLine()
@@ -44,7 +45,7 @@ fun main() {
             } while (column < 3)
 
             println("How many mines (press enter for default value)?")
-            val mines = readLine()?.toIntOrNull()?:3
+            mines = readLine()?.toIntOrNull()?:3
             if (wantLegend) {
                 createLegend(column)
                 }
@@ -52,7 +53,7 @@ fun main() {
     } while (num != "0" && num != "1")
 }
 
-fun makeMenu(): String = "\nWelcome to DEISI Minesweeper\n\n1 - Start New Game\n0 - Exit Game\n"  //Função Correta
+fun makeMenu(): String = "\nWelcome to DEISI Minesweeper\n\n1 - Start New Game\n0 - Exit Game\n"
 
 fun makeTerrain(numLines: Int, numColumns: Int, numMines: Int, showLegend: Boolean = true, withColor: Boolean = false): String = ""
 
@@ -72,9 +73,22 @@ fun isNameValid(name: String?, minLength: Int = 3): Boolean {
     } else return false
 }
 
-//Função isNameValid a compilar corretamente
-
-fun calculateNumMinesForGameConfiguration(numLines: Int, numColumns: Int): Int? = 0
+fun calculateNumMinesForGameConfiguration(numLines: Int, numColumns: Int): Int? {
+    val casasVazias = numLines*numColumns-2
+    if (casasVazias == 1) {
+        return 1
+    } else if (casasVazias in 2..5) {
+        return 2
+    } else if (casasVazias in 6..10) {
+        return 3
+    } else if (casasVazias in 11..20) {
+        return 6
+    } else if (casasVazias in 21..50) {
+        return 10
+    } else if (casasVazias >= 51) {
+        return 15
+    } else return null
+}
 
 fun createLegend(numColumns: Int): String {
     val legendLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -87,6 +101,9 @@ fun createLegend(numColumns: Int): String {
     return legendString
 }
 
-//Função createLegend a compilar corretamente
-
-fun isValidGameMinesConfiguration(numLines: Int, numColumns: Int, numMines: Int): Boolean = false
+fun isValidGameMinesConfiguration(numLines: Int, numColumns: Int, numMines: Int): Boolean {
+    val casasVazias = numLines*numColumns-2
+    if (numMines <= 0 || casasVazias < numMines) {
+        return false
+    } else return true
+}
