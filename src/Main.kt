520eb7fa2e7cf6d84f63b3c359d4eab32aebf432
TunaPT <78658365+TunaPT@users.noreply.github.com>
2021-12-30 18:48:13 +0000

    Merge Manual

--- src/Main.kt
+++ src/Main.kt
@@ -2,56 +2,69 @@ fun main() {
 }
 
 fun test() {
-    val invalid = "Invalid response.\n"
-    var column = 0
-    var line = 0
-    var mines = 0
-    do {
-        println(makeMenu())
-        val num = readLine()
-        if (num != "0" && num != "1") {
-            println(invalid)
-        }
-        if (num == "1") {
-            do {
-                println("Enter player name?")
-                val name = readLine()
-                if (!isNameValid(name, 3)) {
-                    println(invalid)
-                }
-            } while (!isNameValid(name, 3))
+    var sair = false
+    do{
+        val invalid = "Invalid response.\n"
+        var column = 0
+        var line = 0
+        var mines = 0
+        do {
+            println(makeMenu())
+            val num = readLine()
+            if (num != "0" && num != "1") {
+                println(invalid)
+            }
+            if (num == "1") {
+                do {
+                    println("Enter player name?")
+                    val name = readLine()
+                    if (!isNameValid(name, 3)) {
+                        println(invalid)
+                    }
+                } while (!isNameValid(name, 3))
 
-            do {
-                println("Show legend (y/n)?")
-                val legend = readLine()!!
-                if (legend != "y" && legend != "n" && legend != "Y" && legend != "N") {
-                    println(invalid)
-                }
-            } while (legend != "y" && legend != "n" && legend != "Y" && legend != "N")
+                do {
+                    println("Show legend (y/n)?")
+                    val legend = readLine()!!
+                    if (legend != "y" && legend != "n" && legend != "Y" && legend != "N") {
+                        println(invalid)
+                    }
+                } while (legend != "y" && legend != "n" && legend != "Y" && legend != "N")
 
-            do {
-                println("How many lines?")
-                line = readLine()!!.toInt()
-                if (line != 1) {
-                    println(invalid)
-                }
-            } while (line != 1)
+                do {
+                    println("How many lines?")
+                    line = readLine()!!.toInt()
+                    if (line !in 4..9) {
+                        println(invalid)
+                    }
+                } while (line !in 4..9)
 
-            do {
-                println("How many columns?")
-                column = readLine()!!.toInt()
-                if (column < 3) {
-                    println(invalid)
-                }
-            } while (column < 3)
+                do {
+                    println("How many columns?")
+                    column = readLine()!!.toInt()
+                    if (column !in 4..9) {
+                        println(invalid)
+                    }
+                } while (column !in 4..9)
 
-            do {
-                println("How many mines (press enter for default value)?")
-                mines = readLine()?.toIntOrNull() ?: 3
-            } while (!isValidGameMinesConfiguration(line,column,mines))
-            //println(makeTerrain(line, column, mines))
-        }
-    } while (num != "0" && num != "1")
+                do {
+                    println("How many mines (press enter for default value)?")
+                    mines = readLine()?.toIntOrNull() ?: 6
+                } while (!isValidGameMinesConfiguration(line,column,mines))
+                //println(makeTerrain(line, column, mines))
+            }
+            if (num == "1"){
+                println("Prima Qualquer tecla para Continuar")
+                println("Prima 0 para Sair")
+                var saida = readLine()?.toIntOrNull()?:1
+                if (saida == 0){
+                    sair = true
+                }
+            }else{
+                sair = true
+            }
+        } while (num != "0" && num != "1")
+    }while (!sair)
 }
 
 fun makeMenu(): String = "\nWelcome to DEISI Minesweeper\n\n1 - Start New Game\n0 - Exit Game\n"
@@ -155,21 +168,41 @@ fun isEmptyAround(matrixTerrain: Array<Array<Pair<String, Boolean>>>, centerY: I
 
 fun isMovementPValid(currentCoord : Pair<Int, Int>, targetCoord : Pair<Int, Int>): Boolean = false
 
-fun isCoordinateInsideTerrain(coord: Pair<Int, Int>, numColumns: Int, numLines: Int): Boolean = false
+fun isCoordinateInsideTerrain(coord: Pair<Int, Int>, numColumns: Int, numLines: Int): Boolean {
+    if (coord.first < numColumns && coord.second < numLines) {
+        if (coord.first >= 0 && coord.second >= 0) {
+            return true
+        } else {
+            return false
+        }
+    } else {
+        return false
+    }
+}
 
 fun getCoordinates (readText: String?): Pair<Int, Int>? {
     if (readText != null && readText.length == 2) {
-        val first = readText[0].toInt()
-        if (readText[1] == 'A') {
-            return Pair(first,0)
-        } else if (readText[1] == 'B') {
-            return Pair(first,1)
-        } else if (readText[1] == 'C') {
-            return Pair(first,2)
-        } else if (readText[1] == 'D') {
-            return Pair(first,3)
-        } else if (readText[1] == 'E') {
-            return Pair(first,4)
+        var count = 0
+        if (readText[0].isDigit()) {
+            val valor = readText[0].toInt() - 49
+            if (readText[1].isLetter()) {
+                val legendLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                val legendLetter2 = "abcdefghijklmnopqrstuvwxyz"
+                while ((readText[1] != legendLetter[count]) && (readText[1] != legendLetter2[count])) {
+                    if ((count != legendLetter.length-1) && (count != legendLetter2.length-1)) {
+                        count += 1
+                    }
+                }
+                if (valor >= 0) {
+                    return Pair(valor, count)
+                } else {
+                    return null
+                }
+            } else {
+                return null
+            }
+        } else {
+            return null
         }
     }
     return null
